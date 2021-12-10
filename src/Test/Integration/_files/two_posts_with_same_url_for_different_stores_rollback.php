f11b119b48e2c5c7b96e0d923d4ad04850bc6e6a
Simon Sprankel <simon@customgento.com>
2021-12-10 10:46:00 +0100

    Fix fixture rollback, SUP-6451

--- src/Test/Integration/_files/two_posts_with_same_url_for_different_stores_rollback.php
+++ src/Test/Integration/_files/two_posts_with_same_url_for_different_stores_rollback.php
@@ -7,17 +7,17 @@ use Mageplaza\Blog\Api\Data\PostInterface;
 use Mageplaza\Blog\Model\Post;
 use Mageplaza\Blog\Model\ResourceModel\Post as PostResource;
 
-require __DIR__ . '/../../../../../../../dev/tests/integration/testsuite/Magento/Store/_files/second_store.php';
-
-$objectManager  = Bootstrap::getObjectManager();
+$objectManager = Bootstrap::getObjectManager();
 
 /** @var PostResource $postResource */
 $postResource = $objectManager->create(PostResource::class);
-$post         = $objectManager->create(Post::class);
 
-$post1 = $postResource->load($post, 'test1', PostInterface::URL_KEY);
-$post2 = $postResource->load($post, 'test2', PostInterface::URL_KEY);
-/** @var Magento\Framework\Model\AbstractModel $post1 */
-$postResource->delete($post1);
-/** @var Magento\Framework\Model\AbstractModel $post2 */
-$postResource->delete($post2);
+/** @var Post $post */
+$post = $objectManager->create(Post::class);
+$postResource->load($post, 'test1', PostInterface::URL_KEY);
+$postResource->delete($post);
+
+/** @var Post $post */
+$post = $objectManager->create(Post::class);
+$postResource->load($post, 'test2', PostInterface::URL_KEY);
+$postResource->delete($post);
